b3e4bfde12da8e3d19df085c427eb283fcb36bde
Hardson TESSI <hardsontessi2@gmail.com>
2025-03-17 19:24:54 +0100

    initial commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, l) {
+	let result= ""
+	if (l < 0)
+		return "ERROR"
+	for (let i = 0; i < l; i++) {
+		result += str
+	}
+	return result
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+	if (!(Number.isInteger(start) && Number.isInteger(end)) || !(start >= 0 && end >= 0)) {
+		return "ERROR"
+	}
+	if (start > end) {
+		let tmp = start;
+		start = end;
+		end = tmp
+	}
+	return ((end - start + 1) * (start + end)) / 2
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
